67b79d0f8602e59326692420a47e36b22f37991f
João Pedro deV junior/pleno <easypedrosilva@gmail.com>
2023-10-22 01:54:29 -0300

    new update from page/home-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './Components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './Components/menu-title/menu-title.component';
+import { BigCardComponent } from './Components/big-card/big-card.component';
+import { SmallCardComponent } from './Components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
